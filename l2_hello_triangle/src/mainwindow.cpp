5ba4300983e5519aa10efd517547e52a76cdefd6
Your Name <you@example.com>
2024-07-08 20:46:39 -0400

    Fixed formatting

--- l2_hello_triangle/src/mainwindow.cpp
+++ l2_hello_triangle/src/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(std::make_unique<Ui::MainWindow>()) {
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(std::make_unique<Ui::MainWindow>()) {
     ui->setupUi(this);
 }
